0ced18766f936d3a69f3de52f0d2efa21fb9b5ce
JGP-1208 <josgp1208@gmail.com>
2026-03-27 16:15:23 -0600

    Add nodo creation and destruction functions
    Implemented functions to create and destroy a Nodo.

--- nodo/nodo.c
+++ nodo/nodo.c
@@ -1,11 +1,22 @@
 #include <stdlib.h>
 #include "nodo.h"
 
-Nodo* nodo_crear(int dato)
-{
-    // Todo: implementar la función }
 
-void nodo_destruir(Nodo* nodo)
-{
-    // Todo: implementar la función 
+Nodo* nodo_crear(int dato){
+
+
+   Nodo *n;
+   n = (Nodo*)malloc(sizeof(Nodo));
+   n -> next = NULL;
+   n -> dato = dato;
+   return n;
+}
+
+
+void nodo_destruir(Nodo* nodo){
+
+
+   if(n -> next == NULL){
+       free(n);
+   }
 }
